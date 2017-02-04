581efd6c03647d429b98b639a1bc075517685ea0
Frederick Liu <frederick0329@gmail.com>
2017-02-04 13:46:14 -0500

    add op

--- crawl.py
+++ crawl.py
@@ -6,13 +6,19 @@ import argparse
 if __name__ == '__main__':
 
 
+
     parser = argparse.ArgumentParser()
     parser.add_argument('-l','--lang', help='which language to crawl', required=True)
     parser.add_argument('-n','--num', type=int, help='the number of pages to crawl for each category', required=True)
+    parser.add_argument('-d','--dir', type=int, help='directory to save the crawled files', required=True)
     args = vars(parser.parse_args())
     lang = args['lang']
     num = args['num']
+    directory = args['dir']
 
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    
     #Crawling wiki
     print "Crawling..." + lang + " wikipedia"
     os.system('python WikiCrawler.py -l ' + lang + ' -n ' + str(num))
